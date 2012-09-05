7e638c341eda38b545e032b7d0b680b32644be05
Sanjay Krishnan <sanjaykrishn@gmail.com>
2012-09-05 11:07:52 -0700

    Generalizing some of the settings file, removing binaries

--- prop30/settings.py
+++ prop30/settings.py
@@ -17,9 +17,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'qim_prop30_2',                      # Or path to database file if using sqlite3.
-        'USER': 'qim_prop30',                      # Not used with sqlite3.
-        'PASSWORD': 'M689ueapzzqFnU82',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
